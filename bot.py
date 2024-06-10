deffecb0c962e477f765b21349cde8910966205f
cpanel10x <me@vutn.net>
2024-06-10 09:05:08 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 API_URL = os.getenv('API_URL')
 
 # Thiết lập logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levellevel)s - %(message)s',
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
