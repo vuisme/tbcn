b5c6b892b9dd49fd9beb07f05909f39cdeae6bcd
cpanel10x <me@vutn.net>
2024-06-15 20:55:35 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -76,6 +76,7 @@ async def handle_message(update: Update, context: CallbackContext) -> None:
                 if response.status_code == 200:
                     data = response.json()
                     img_urls = data.get('images', []) + data.get('skubaseImages', []) + data.get('video', [])
+                    logger.info(img_urls)
                     cleaned_urls = list(set(clean_image_url(url) for url in img_urls))
                     await download_and_send_media(update, cleaned_urls, reply_func, reply_media_group_func)
                 else:
